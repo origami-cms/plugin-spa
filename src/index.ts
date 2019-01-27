2d7cc634826e1bc819c8e7d8a8f861161ccd54d3
Tristan Matthias <hello@tristanmatthias.com>
2019-01-27 16:51:16 +1100

    feat: upgraded to support new Origami version

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import { Route } from '@origami/core-lib';
+import { Server } from '@origami/core-server';
 import * as path from 'path';
-import Server, {Route} from 'origami-core-server';
-
 export interface SPAOptions {
     fallback: string;
 }
@@ -9,9 +9,20 @@ module.exports = (server: Server, options: SPAOptions) => {
     const r = new Route('*')
         .position('post-render')
         .use(async (req, res, next) => {
-            const body = res.body || res.text || res.data || res.responseCode;
-            if (!res.headersSent && !body) res.sendFile(path.resolve(process.cwd(), options.fallback));
-            else next();
+            const body =
+                res.locals.content.get() ||
+                // @ts-ignore Support legacy code
+                res.body ||
+                // @ts-ignore Support legacy code
+                res.text ||
+                // @ts-ignore Support legacy code
+                res.data ||
+                // @ts-ignore Support legacy code
+                res.responseCode;
+
+            if (!res.headersSent && !body) {
+                res.sendFile(path.resolve(process.cwd(), options.fallback));
+            } else next();
         });
 
     server.useRouter(r);
